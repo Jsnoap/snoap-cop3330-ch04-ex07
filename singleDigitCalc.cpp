f46916852a14a180b84a92068b619c385d6ce11b
jsnoap <jordan.snoap@gmail.com>
2021-11-27 11:54:24 -0500

    Added Comment

--- singleDigitCalc.cpp
+++ singleDigitCalc.cpp
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Jordan Snoap
+ */
+
 #include "std_lib_facilities.h"
 
 // Performs operation with values passed and prints out answer statements
